1af02db96ffbe602f6bcde5a07016b2a34906049
0xirvan <darknet1255@gmail.com>
2025-01-11 06:04:22 +0700

    Range dan close

---
+++ chapter-A.34-range-and-closes/go.mod
@@ -0,0 +1,3 @@
+module range_and_closes
+
+go 1.23.4
---
+++ chapter-A.34-range-and-closes/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func sendMessage(ch chan<- string) {
+	for i := 0; i < 20; i++ {
+		ch <- fmt.Sprintf("Data %d", i)
+	}
+	close(ch)
+}
+
+func printMessage(ch <-chan string) {
+	for message := range ch {
+		fmt.Println(message)
+	}
+}
+
+func main() {
+	var messages = make(chan string)
+	go sendMessage(messages)
+	printMessage(messages)
+}
